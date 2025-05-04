36eb2e37a186514d97434c8efb6ccea921a960cd
IParamonikhin <paramonikhin.ivan@yandex.ru>
2025-05-04 16:31:10 +0300

    Initial Commit

---
+++ CarClassifier/CarClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarClassifierApp.swift
+//  CarClassifier
+//
+//  Created by Иван on 04.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
